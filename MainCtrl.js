dec9d102c55012415f97eb0eb430a50697c5cbdf
Matt Higbee <mhigbee@hotmail.com>
2016-06-11 13:35:42 -0600

    first commit

---
+++ MainCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by matthigbee on 6/11/16.
+ */
+angular.module('marvelApi').controller('MainCtrl', function($scope,dataService){
+    $scope.test = "test";
+
+});
+
+
+// myApp.controller('MyController', function($scope)
---
+++ dataService.js
@@ -0,0 +1,7 @@
+/**
+ * Created by matthigbee on 6/11/16.
+ */
+angular.module('marvelApi').service('dataService', function($http){
+
+
+});
